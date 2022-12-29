865bd32e64f52fb1d76df8c3f213a06082ad40a5
kimkuns <>
2022-12-29 11:36:46 +0900

    수정함

--- server/server.js
+++ server/server.js
@@ -5,19 +5,16 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-    origin: "https://dblockfilter.com/",
-    methods: ["GET", "POST"],
-    credentials: true,
-}));
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(cors());
 app.listen(8080, () => {
     console.log('Server is running on port 8080.');
 });
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 /* All Files - React */
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  });
+});
   
